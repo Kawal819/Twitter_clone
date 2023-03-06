65cff16c7736371341122ee4176a9e795cb6c8a0
Kawal819 <singhkawal365@gmail.com>
2023-03-06 15:26:29 +0530

    Backend completed

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../verifyToken.js';
-import { createTweet ,deleteTweet, likeOrDislike } from '../controllers/tweet.js';
+import { createTweet ,deleteTweet, likeOrDislike,getAllTweets ,getUserTweets, getExploreTweets } from '../controllers/tweet.js';
 const router = express.Router();
 
 //Create a tweet
@@ -12,5 +12,13 @@ router.delete("/:id",verifyToken,deleteTweet);
 //Like or dislike a tweet
 router.put('/:id/like',likeOrDislike);
 
+//Get all timeline tweets
+router.get('/timeline/:id',getAllTweets);
+
+//Get user Tweet only
+router.get('/user/all/:id',getUserTweets);
+
+//explore
+router.get('/explore',getExploreTweets);
 
 export default router;
